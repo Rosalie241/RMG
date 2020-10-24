b775195257b0aa0e3e4e5dbc3d295b19782f6d2c
Rosalie Wanders <rosalie@mailbox.org>
2020-10-24 16:50:49 +0200

    license

--- Source/RMG/UserInterface/Dialog/SettingsDialog.cpp
+++ Source/RMG/UserInterface/Dialog/SettingsDialog.cpp
@@ -1,3 +1,12 @@
+/*
+ * Rosalie's Mupen GUI - https://github.com/Rosalie241/RMG
+ *  Copyright (C) 2020 Rosalie Wanders <rosalie@mailbox.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 #include "SettingsDialog.hpp"
 
 using namespace UserInterface::Dialog;
