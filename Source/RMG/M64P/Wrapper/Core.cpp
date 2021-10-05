756564bd32b922b9bc4206a62b9e7cd998c71a4e
Rosalie Wanders <rosalie@mailbox.org>
2021-10-05 10:46:30 +0200

    Core: use different command in SetSaveSlot

--- Source/RMG/M64P/Wrapper/Core.cpp
+++ Source/RMG/M64P/Wrapper/Core.cpp
@@ -548,10 +548,10 @@ bool Core::SetSaveSlot(int slot)
 {
     m64p_error ret;
 
-    ret = M64P::Core.DoCommand(M64CMD_STATE_SET_SLOT, slot, NULL);
+    ret = M64P::Core.DoCommand(M64CMD_CORE_STATE_SET, M64CORE_SAVESTATE_SLOT, &slot);
     if (ret != M64ERR_SUCCESS)
     {
-        this->error_Message = "Core::SetSaveSlot M64P::Core.DoCommand(M64CMD_STATE_SET_SLOT) Failed: ";
+        this->error_Message = "Core::SetSaveSlot M64P::Core.DoCommand(M64CMD_CORE_STATE_SET, M64CORE_SAVESTATE_SLOT) Failed: ";
         this->error_Message += M64P::Core.ErrorMessage(ret);
     }
 
