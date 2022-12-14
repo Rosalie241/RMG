8661273568751dfc506189ff2411395bd5cef5e0
Rosalie Wanders <rosalie@mailbox.org>
2022-12-14 19:24:51 +0100

    Script: update DownloadStyles.sh

--- Source/Script/DownloadStyles.sh
+++ Source/Script/DownloadStyles.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 script_dir="$(dirname "$0")"
 toplvl_dir="$(realpath "$script_dir/../../")"
 tmp_dir="$toplvl_dir/Build/RMG-Dark-Style/"
@@ -8,9 +8,6 @@ target_styles_dir="$toplvl_dir/Bin/Release/Data/Styles/"
 mkdir -p "$target_styles_dir"
 rm -rf "$tmp_dir"
 
-git clone https://github.com/Rosalie241/Dolphin-Dark-Mode/ "$tmp_dir"
-pushd "$tmp_dir"
-git checkout -f cb07df26c49408a3e1122489d5a66ce2c70963fd
-popd
+git clone https://github.com/Rosalie241/Dolphin-Dark-Mode/ "$tmp_dir" -b RMG
 
 cp -r "$tmp_dir/Data/Styles"/* "$target_styles_dir/"
