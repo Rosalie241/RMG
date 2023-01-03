7b3293341b343ddd95c4cc5f4f003c91974b5f54
Rosalie Wanders <rosalie@mailbox.org>
2023-01-03 13:50:57 +0100

    RMG: check visibility of OGLWidget in VidExt_OglSetup()

--- Source/RMG/VidExt.cpp
+++ Source/RMG/VidExt.cpp
@@ -36,7 +36,8 @@ static void VidExt_OglSetup(void)
 {
     l_EmuThread->on_VidExt_SetupOGL(l_SurfaceFormat, QThread::currentThread());
 
-    while (!l_OGLWidget->GetContext()->isValid())
+    while (!l_OGLWidget->isVisible() ||
+           !l_OGLWidget->GetContext()->isValid())
     {
         continue;
     }
