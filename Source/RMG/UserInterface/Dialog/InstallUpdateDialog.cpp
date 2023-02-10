7c59a520aae45eefde89f300db58b9b263df0db5
Rosalie Wanders <rosalie@mailbox.org>
2023-02-10 20:52:03 +0100

    RMG: improve updater script for windows

--- Source/RMG/UserInterface/Dialog/InstallUpdateDialog.cpp
+++ Source/RMG/UserInterface/Dialog/InstallUpdateDialog.cpp
@@ -39,16 +39,40 @@ void InstallUpdateDialog::install(void)
 
     QString appPath = QCoreApplication::applicationDirPath();
     QString appPid  = QString::number(QCoreApplication::applicationPid());
+    QString logPath = appPath + "/Cache/updater.log";
 
 #ifdef _WIN32
+    // convert paths to use the right path seperator
+    this->temporaryDirectory = QDir::toNativeSeparators(this->temporaryDirectory);
+    fullFilePath             = QDir::toNativeSeparators(fullFilePath);
+    appPath                  = QDir::toNativeSeparators(appPath);
+    logPath                  = QDir::toNativeSeparators(logPath);
+
+    // remove log file when it exists
+    QFile qLogFile(logPath);
+    if (qLogFile.exists())
+    {
+        qLogFile.remove();
+    }
+
+    QString outputToLogLine = " >> \"" + logPath + "\" 2>&1";
+
     if (this->filename.endsWith(".exe"))
     {
         this->label->setText("Executing " + this->filename + "...");
-        QStringList scriptLines = 
+        QStringList scriptLines =
         {
             "@echo off",
-            "taskkill /F /PID:" + appPid,
-            "\"" + fullFilePath + "\" /CLOSEAPPLICATIONS /SILENT /DIR=\"" + appPath + "\"",
+            "(",
+            "   echo == Attemping to kill PID " + appPid + outputToLogLine,
+            "   taskkill /F /PID:"              + appPid + outputToLogLine,
+            "   echo == Attemping to start \'" + fullFilePath + "\'"                                    + outputToLogLine,
+            "   \"" + fullFilePath + "\" /CLOSEAPPLICATIONS /NOCANCEL /SILENT /DIR=\"" + appPath + "\"" + outputToLogLine,
+            ")",
+            "IF NOT ERRORLEVEL 0 (",
+            "   start \"\" cmd /c \"echo Rosalie's Mupen GUI failed to update, check the updater.log file in the Cache directory for more information && pause\"",
+            ")",
+            // remove temporary directory at last
             "rmdir /S /Q \"" + this->temporaryDirectory + "\"",
         };
         this->writeAndRunScript(scriptLines);
@@ -77,20 +101,32 @@ void InstallUpdateDialog::install(void)
         this->showErrorMessage("CoreUnzip() Failed!", QString::fromStdString(CoreGetError()));
         this->reject();
         return;
-    }    
+    }
 
     this->label->setText("Executing update script...");
     this->progressBar->setValue(100);
 
 #ifdef _WIN32
+    extractDirectory = QDir::toNativeSeparators(extractDirectory);
+
     QStringList scriptLines = 
     {
         "@echo off",
-        "del /F /Q \""      + fullFilePath + "\"",
-        "taskkill /F /PID:" + appPid,
-        "xcopy /S /Y /I \"" + extractDirectory + "\\*\" \"" + appPath + "/\"",
-        "start \"\" \""     + appPath + "\\RMG.exe\"",
-        "rmdir /S /Q \""    + this->temporaryDirectory + "\"",
+        "(",
+        "   echo == Attempting to remove \'" + fullFilePath + "\'" + outputToLogLine,
+        "   del /F /Q \""                    + fullFilePath + "\"" + outputToLogLine,
+        "   echo == Attemping to kill PID " + appPid               + outputToLogLine,
+        "   taskkill /F /PID:"              + appPid               + outputToLogLine,
+        "   echo == Attemping to copy \'" + extractDirectory + "\' to \'" + appPath + "\'"  + outputToLogLine,
+        "   xcopy /S /Y /I \""            + extractDirectory + "\\*\" \"" + appPath + "\""  + outputToLogLine,
+        "   echo == Attemping to start \'" + appPath + "\\RMG.exe\'"           + outputToLogLine,
+        "   start \"\" \""                 + appPath + "\\RMG.exe\""           + outputToLogLine,
+        ")",
+        "IF NOT ERRORLEVEL 0 (",
+        "   start \"\" cmd /c \"echo Rosalie's Mupen GUI failed to update, check the updater.log file in the Cache directory for more information && pause\"",
+        ")",
+        // remove temporary directory at last
+        "rmdir /S /Q \"" + this->temporaryDirectory + "\"",
     };
     this->writeAndRunScript(scriptLines);
 #else // Linux
