4823e39c76e1871b2b10f8c5ca0cb9f44ba20f3c
RetraCarteR <cbmclean98@gmail.com>
2023-03-04 15:47:28 -0600

    Implement Requested Changes

--- Source/RMG-Core/RomHeader.cpp
+++ Source/RMG-Core/RomHeader.cpp
@@ -22,6 +22,97 @@
 #include "Rom.hpp"
 
 //
+// Local Functions
+//
+
+static std::string generateGameID(m64p_rom_header header)
+{
+    std::string gameID;
+
+    if (header.Manufacturer_ID == 0)
+    {
+        return "????";
+    }
+
+    gameID.push_back(char(ntohl(header.Manufacturer_ID)));
+    gameID.push_back(char(header.Cartridge_ID % 256));
+    gameID.push_back(char(header.Cartridge_ID / 256));
+    gameID.push_back(char(header.Country_code));
+
+    return gameID;
+}
+
+static std::string generateRegionString(char countryCode)
+{
+    std::string region;
+
+    switch (countryCode)
+    {
+        case 'A':
+            region = "Region-Free";
+            break;
+        case 'B':
+            region = "Brazil";
+            break;
+        case 'C':
+            region = "China";
+            break;
+        case 'D':
+            region = "Germany";
+            break;
+        case 'E':
+            region = "North America";
+            break;
+        case 'F':
+            region = "France";
+            break;
+        case 'G':
+            region = "Gateway 64 (NTSC)";
+            break;
+        case 'H':
+            region = "Netherlands";
+            break;
+        case 'I':
+            region = "Italy";
+            break;
+        case 'J':
+            region = "Japan";
+            break;
+        case 'K':
+            region = "Korea";
+            break;
+        case 'L':
+            region = "Gateway 64 (PAL)";
+            break;
+        case 'N':
+            region = "Canada";
+            break;
+        case 'P':
+        case 'X':
+            region = "Europe/Australia";
+            break;
+        case 'S':
+            region = "Spain";
+            break;
+        case 'U':
+            region = "Australia";
+            break;
+        case 'W':
+            region = "Scandanavia";
+            break;
+        case 'Y':
+        case 'Z':
+            region = "Europe";
+            break;
+        default:
+            region = "Unknown";
+            break;
+    }
+
+    return region;
+}
+
+//
 // Exported Functions
 //
 
@@ -49,124 +140,8 @@ bool CoreGetCurrentRomHeader(CoreRomHeader& header)
     header.CRC2        = ntohl(m64p_header.CRC2);
     header.CountryCode = m64p_header.Country_code;
     header.Name        = CoreConvertStringEncoding((char*)m64p_header.Name, CoreStringEncoding::Shift_JIS);
-
-    header.GameID.clear();
-    header.GameID.push_back(char(ntohl(m64p_header.Manufacturer_ID)));
-    header.GameID.push_back(char(m64p_header.Cartridge_ID % 256));
-    header.GameID.push_back(char(m64p_header.Cartridge_ID / 256));
-    header.GameID.push_back(char(header.CountryCode));
-
-    if (header.GameID[0] == '\0')
-    {
-        header.GameID = "????";
-    }
-    
-    switch (header.CountryCode)
-    {
-        case 65:
-        // A
-        header.Region = "Region-Free";
-        break;
-
-        case 66:
-        // B
-        header.Region = "Brazil";
-        break;
-
-        case 67:
-        // C
-        header.Region = "China";
-        break;
-
-        case 68:
-        // D
-        header.Region = "Germany";
-        break;
-
-        case 69:
-        // E
-        header.Region = "North America";
-        break;
-
-        case 70:
-        // F
-        header.Region = "France";
-        break;
-
-        case 71:
-        // G
-        header.Region = "Gateway 64 (NTSC)";
-        break;
-
-        case 72:
-        // H
-        header.Region = "Netherlands";
-        break;
-
-        case 73:
-        // I
-        header.Region = "Italy";
-        break;
-
-        case 74:
-        // J
-        header.Region = "Japan";
-        break;
-
-        case 75:
-        // K
-        header.Region = "Korea";
-        break;
-
-        case 76:
-        // L
-        header.Region = "Gateway 64 (PAL)";
-        break;
-
-        case 78:
-        // N
-        header.Region = "Canada";
-        break;
-
-        case 80:
-        // P
-        header.Region = "Europe/Australia";
-        break;
-
-        case 83:
-        // S
-        header.Region = "Spain";
-        break;
-
-        case 85:
-        // U
-        header.Region = "Australia";
-        break;
-
-        case 87:
-        // W
-        header.Region = "Scandanavia";
-        break;
-
-        case 88:
-        // X
-        header.Region = "Europe/Australia";
-        break;
-
-        case 89:
-        // Y
-        header.Region = "Europe";
-        break;
-
-        case 90:
-        // Z
-        header.Region = "Europe";
-        break;
-
-        default:
-        header.Region = "Unknown";
-        break;
-    }
+    header.GameID      = generateGameID(m64p_header);
+    header.Region      = generateRegionString((char)header.CountryCode);
 
     return true;
 }
