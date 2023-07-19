06164d41b10707838444095da63841248d6f28f6
Rosalie Wanders <rosalie@mailbox.org>
2023-07-19 10:59:53 +0200

    3rdParty: add mupen64plus-rsp-parallel

---
+++ Source/3rdParty/mupen64plus-rsp-parallel/rsp_disasm.hpp
@@ -0,0 +1,13 @@
+#ifndef RSP_DISASM_HPP_
+#define RSP_DISASM_HPP_
+
+#include <string>
+#include <cstdint>
+
+namespace RSP
+{
+std::string disassemble(uint32_t pc, uint32_t instr);
+const char *register_name(unsigned reg_index);
+}
+
+#endif
