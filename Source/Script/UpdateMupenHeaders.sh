8ad727b33f5dfbdded8ae0d0f5e19876e843943e
Rosalie Wanders <rosalie@mailbox.org>
2023-08-01 15:43:22 +0200

    Script: fix UpdateMupenHeaders.sh

--- Source/Script/UpdateMupenHeaders.sh
+++ Source/Script/UpdateMupenHeaders.sh
@@ -2,7 +2,7 @@
 set -ex
 script_dir="$(dirname "$0")"
 toplvl_dir="$(realpath "$script_dir/../../")"
-m64psrc_dir="$toplvl_dir/Build/Release/Source/3rdParty/mupen64plus-core/src"
+m64psrc_dir="$toplvl_dir/Source/3rdParty/mupen64plus-core/src"
 header_dir="$toplvl_dir/Source/RMG-Core/m64p/api"
 
 for h in common config debugger frontend plugin types vidext
