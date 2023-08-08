37ee620f1895055bc65c1b5ee1902eeec1e5f8a0
busywait <busywait@users.noreply.github.com>
2023-08-08 11:33:36 +0100

    Don't strip (debug symbols) from debug builds
    mend

--- Source/Script/Build.sh
+++ Source/Script/Build.sh
@@ -18,7 +18,13 @@ mkdir -p "$build_dir"
 cmake -S "$toplvl_dir" -B "$build_dir" -DCMAKE_BUILD_TYPE="$build_config" -DPORTABLE_INSTALL=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G "Ninja"
 
 cmake --build "$build_dir" --parallel "$threads"
-cmake --install "$build_dir" --strip --prefix="$toplvl_dir"
+
+strip_args=" --strip "
+if [ "$build_config"="Debug" ]
+then
+    strip_args=" "
+fi
+cmake --install "$build_dir" $strip_args --prefix="$toplvl_dir"
 
 if [[ $(uname -s) = *MINGW64* ]]
 then
