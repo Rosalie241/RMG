0e2a1ba3d9f9b32350968fd6b8c81300f86b74a2
Rosalie Wanders <rosalie@mailbox.org>
2023-08-27 19:31:46 +0200

    RMG: add license header to QtKeyToSdl2Key.cpp

--- Source/RMG/Utilities/QtKeyToSdl2Key.hpp
+++ Source/RMG/Utilities/QtKeyToSdl2Key.hpp
@@ -1,3 +1,12 @@
+/*
+ * Rosalie's Mupen GUI - https://github.com/Rosalie241/RMG
+ *  Copyright (C) 2020 Rosalie Wanders <rosalie@mailbox.org>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 3.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef QTKEYTOSDL2KEY_HPP
 #define QTKEYTOSDL2KEY_HPP
 
@@ -5,8 +14,8 @@
 
 namespace Utilities
 {
-int QtKeyToSdl2Key(int);
-int QtModKeyToSdl2ModKey(Qt::KeyboardModifiers);
+int QtKeyToSdl2Key(int key);
+int QtModKeyToSdl2ModKey(Qt::KeyboardModifiers modifiers);
 } // namespace Utilities
 
 #endif // QTKEYTOSDL2KEY_HPP
