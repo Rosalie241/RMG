5a443348cc396ab53d718c0d2779c181da9d6d6a
Rosalie Wanders <rosalie@mailbox.org>
2024-12-25 13:18:14 +0100

    RMG: make sendPushButton default automatically in NetplaySessionDialog

--- Source/RMG/UserInterface/Dialog/Netplay/NetplaySessionDialog.cpp
+++ Source/RMG/UserInterface/Dialog/Netplay/NetplaySessionDialog.cpp
@@ -120,6 +120,7 @@ void NetplaySessionDialog::on_webSocket_textMessageReceived(QString message)
 void NetplaySessionDialog::on_chatLineEdit_textChanged(QString text)
 {
     this->sendPushButton->setEnabled(!text.isEmpty() && text.size() <= 256);
+    this->sendPushButton->setDefault(this->sendPushButton->isEnabled());
 }
 
 void NetplaySessionDialog::on_sendPushButton_clicked(void)
