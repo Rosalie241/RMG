70cf93a0e9f68278300a74202edc3f3e9d9b698e
Rosalie Wanders <rosalie@mailbox.org>
2025-02-06 21:24:41 +0100

    3rdParty: backport some patches from simple64 to mupen64plus-core

--- Source/3rdParty/mupen64plus-core/src/device/r4300/cp0.h
+++ Source/3rdParty/mupen64plus-core/src/device/r4300/cp0.h
@@ -173,11 +173,12 @@ struct interrupt_handler
     void (*callback)(void*);
 };
 
-enum { CP0_INTERRUPT_HANDLERS_COUNT = 16 };
+enum { CP0_INTERRUPT_HANDLERS_COUNT = 17 };
 
 enum {
     INTR_UNSAFE_R4300 = 0x01,
-    INTR_UNSAFE_RSP = 0x02,
+    INTR_UNSAFE_RSP   = 0x02,
+    INTR_UNSAFE_RDP   = 0x04,
 };
 
 struct cp0
