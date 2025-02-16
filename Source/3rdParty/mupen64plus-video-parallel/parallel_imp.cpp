6306d7a96eefee1eb0c38cef51a3953937142534
Rosalie Wanders <rosalie@mailbox.org>
2025-02-16 01:06:41 +0100

    3rdParty: experimental changes to mupen64plus-video-parallel

--- Source/3rdParty/mupen64plus-video-parallel/parallel_imp.cpp
+++ Source/3rdParty/mupen64plus-video-parallel/parallel_imp.cpp
@@ -28,10 +28,6 @@ bool vk_synchronous, vk_divot_filter, vk_gamma_dither;
 bool vk_vi_aa, vk_vi_scale, vk_dither_filter;
 bool vk_interlacing;
 
-static uint64_t rdp_sync_signal;
-static uint64_t last_frame_counter;
-static uint64_t frame_counter;
-
 static const unsigned cmd_len_lut[64] = {
 	1, 1, 1, 1, 1, 1, 1, 1, 4, 6, 12, 14, 12, 14, 20, 22,
 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,  1,  1,  1,  1,  1,
@@ -259,7 +255,6 @@ void vk_rasterize()
 	render_frame(device);
 	(*render_callback)(1);
 	wsi->end_frame();
-	frame_counter++;
 	wsi->begin_frame();
 }
 
@@ -446,15 +441,7 @@ void vk_process_commands()
 			// For synchronous RDP:
 			if (vk_synchronous)
 			{
-				if (frame_counter != last_frame_counter) // Only sync once per frame
-				{
-					rdp_sync_signal = processor->signal_timeline();
-					last_frame_counter = frame_counter;
-				}
-				else
-				{
-					rdp_sync_signal = 0;
-				}
+				processor->wait_for_timeline(processor->signal_timeline());
 			}
 
 			*gfx.MI_INTR_REG |= DP_INTERRUPT;
@@ -475,13 +462,6 @@ void vk_resize()
 {
 	MupenWSIPlatform* platform = (MupenWSIPlatform*)&wsi->get_platform();
 	platform->do_resize();
-}
-
-
-void vk_full_sync()
-{
-	if (vk_synchronous && rdp_sync_signal)
-		processor->wait_for_timeline(rdp_sync_signal);
 }
 
 void vk_destroy()
@@ -576,8 +556,5 @@ bool vk_init()
 	processor->set_quirks(quirks);
 
 	wsi->begin_frame();
-
-	last_frame_counter = 0;
-	frame_counter = 0;
 	return true;
 }
