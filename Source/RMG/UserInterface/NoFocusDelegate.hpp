64d0a90b829e9a809365fd13ae7f10e71b8e57ad
Rosalie Wanders <rosalie@mailbox.org>
2025-03-13 22:07:12 +0100

    RMG: update copyright year

--- Source/RMG/UserInterface/NoFocusDelegate.hpp
+++ Source/RMG/UserInterface/NoFocusDelegate.hpp
@@ -1,6 +1,6 @@
 /*
  * Rosalie's Mupen GUI - https://github.com/Rosalie241/RMG
- *  Copyright (C) 2020 Rosalie Wanders <rosalie@mailbox.org>
+ *  Copyright (C) 2020-2025 Rosalie Wanders <rosalie@mailbox.org>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License version 3.
